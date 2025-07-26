2a9045681fe7e7c3e32e44de6f9f6d1ecb646421
Devyn Johnson <dljohnson1313@gmail.com>
2025-07-26 12:47:50 -0500

    Updating authenticateToken middleware and auth.js files to properly send JWT to frontend

--- Get2KnowMe/server/src/middleware/authenticateTokenMiddleware.js
+++ Get2KnowMe/server/src/middleware/authenticateTokenMiddleware.js
@@ -1,14 +1,2 @@
 import { authenticateToken } from '../utils/auth.js';
-
-export default function authenticateTokenMiddleware(req, res, next) {
-  try {
-    const { user } = authenticateToken(req);
-    if (!user) {
-      return res.status(401).json({ message: 'Authentication required' });
-    }
-    req.user = user;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
-}
+export default authenticateToken;
